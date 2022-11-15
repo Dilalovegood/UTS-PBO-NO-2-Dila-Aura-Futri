813cbbc35d3f8dfd7f8ff8f0196614a34ed4c96a
Dilalovegood <91135063+Dilalovegood@users.noreply.github.com>
2022-11-15 15:15:16 +0700

    Add files via upload

---
+++ MainLingkaran.java
@@ -0,0 +1,12 @@
+
+package UtsJava;
+
+import lingkaran.Lingkaran;
+
+public class MainLingkaran {
+    public static void main(String[] args) {
+        Lingkaran x = new SetengahLingkaran();
+        LuasSetengah y = new LuasSetengah(x);
+   
+    }
+}
